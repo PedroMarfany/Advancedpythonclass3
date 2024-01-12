73e8e004c129dd56f2d81a017d9553459baebb5d
PedroMarfany <156207142+PedroMarfany@users.noreply.github.com>
2024-01-12 16:07:56 +0100

    New Class for filtering

--- scripts/repo_first_scipt.py
+++ scripts/repo_first_scipt.py
@@ -1,20 +1,36 @@
-""""
-Script to Make updates in Github
 """
+Script to make updates in github
+"""
+
+import pandas as pd
 import click
-import pandas as pandas
 
-click.command(short_help= "Parser to import dataset")
-click.option("-f", "--filename", required = True, help = "File to import")
-def main():
+class FilteringClass:
     """
-    Main Function
+    Class for filtering
     """
 
-    pass
+    def _init_(self,df):
+        self.df = df
 
-    df = pd.read_csv(filename)
-    print(df.shape)
+    def filter_price(self, price):
+        """
+        Filter price
+        """
+        return self.df[self.df['Price Starting With ($)'] < price]
 
- if _name_ == "_main_":
+@click.command(short_help='Parser to import datset')
+@click.option('-f', '--file_name', required=True, help='File to import')
+def main(file_name):
+    """
+    Main function
+    """
+    df = pd.read_csv(file_name)
+    
+    
+    result = FilteringClass(df).filter_price(12)
+    
+    print(result.shape)
+
+if _name_ == "_main_":
     main()
