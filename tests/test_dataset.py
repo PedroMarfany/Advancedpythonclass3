900a34dfea0517d7c12d984bc0caf42415e7f644
PedroMarfany <156207142+PedroMarfany@users.noreply.github.com>
2024-01-17 19:08:58 +0100

    Correction of errors

---
+++ tests/test_dataset.py
@@ -0,0 +1,31 @@
+"""
+test loading of the dataset
+"""
+
+import unittest
+from scripts.repo_first_script import load_dataset
+
+
+class TestDataset(unittest.TestCase):
+    """
+    class to test the dataset input in different ways
+    """
+    
+    def setUp(self):
+        """
+        path to dataset
+        """
+        self.path = "dataset/BooksDatasetClean.cdsgssv"
+
+
+    def test_extension_fail(self):
+        """
+        test for the extension of the dataset
+        """
+        with self.assertRaises(TypeError):
+            load_dataset(self.path)
+
+
+if _name_ == "_main_":
+    unittest()
+
